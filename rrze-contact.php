03ccd3984864852206b9116e1b3e3e55dc2ddce9
Benjamin Klemencic <benjamin.klemencic@fau.de>
2022-10-20 18:12:23 +0200

    fixes #5

--- rrze-contact.php
+++ rrze-contact.php
@@ -4,7 +4,7 @@
  * Plugin Name:     RRZE Contact
  * Plugin URI:      https://github.com/RRZE-Webteam/rrze-contact
  * Description:     Einbindung von Daten aus Contact
- * Version:         0.0.48
+ * Version:         0.0.49
  * Author:          RRZE-Webteam
  * Author URI:      https://blogs.fau.de/webworking/
  * License:         GNU General Public License v3
